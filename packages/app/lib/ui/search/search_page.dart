228ce66aea53a0d883e7087204e7c9ece402764a
wjh <wjh@travelsky.com.cn>
2024-03-29 16:59:54 +0800

    refresh doing

--- packages/app/lib/ui/search/search_page.dart
+++ packages/app/lib/ui/search/search_page.dart
@@ -1,12 +1,12 @@
+import 'dart:async';
 
 import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clearmind_archetype_app/app.dart';
-import 'package:resources/resources.dart';
+import 'package:flutter_clearmind_widget_external/components/refresh/easy_refresh/easy_refresh.dart';
 
 import '../../app.dart';
 import 'bloc/search.dart';
-
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -20,20 +20,35 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends BasePageState<SearchPage, SearchBloc> {
   @override
   Widget buildPage(BuildContext context) {
+    // return CommonScaffold(
+    //   body: Center(
+    //     child: ElevatedButton(
+    //       style: ButtonStyle(
+    //         backgroundColor: MaterialStateProperty.all(AppColors.current.primaryColor),
+    //       ),
+    //       onPressed: () {
+    //         navigator.push(AppRouteInfo.login(), useRootNavigator: true);
+    //       },
+    //       child: Text(
+    //         S.current.login,
+    //         style: AppTextStyles.s14w400Primary(),
+    //       ),
+    //     ),
+    //   ),
+    // );
     return CommonScaffold(
-      body: Center(
-        child: ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(AppColors.current.primaryColor),
-          ),
-          onPressed: () {
-            navigator.push(AppRouteInfo.login(), useRootNavigator: true);
+      body: EasyRefreshComponent<User>(
+        controller: EasyRefreshCustomController(
+          scrollController: null,
+          dataLoader: (PageRequestEntity request) async {
+            final completer = Completer<PagingDataEntity<User>>();
+            // todo
+            return await completer.future;
           },
-          child: Text(
-            S.current.login,
-            style: AppTextStyles.s14w400Primary(),
-          ),
         ),
+        widgetBuilder: (data) {
+          return Text(data.email);
+        },
       ),
     );
   }
